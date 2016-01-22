37bea35d5891e481950ddea11ed2650c0c27a958
gauthamip <gauthamip@missionrnd.com>
2016-01-22 17:27:43 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -18,9 +18,42 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 */
 
 #include <stddef.h>
+#include<stdio.h>
 
 
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
+	if (str == NULL)
+	{
+		*vowels = 0;
+		*consonants = 0;
+	}
+	else
+	{
+		int vlist[10] = { 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U' };
+		int i = 0, j, v = 0, c = 0, flag = 0;
+		while (str[i] != '\0')
+		{
+			char ch = str[i];
+			flag = 0;
+			if ((ch >= 65 && ch <= 90) || (ch >= 97 && ch <= 122))
+			{
+				for (j = 0; j < 10; j++)
+				{
+					if (str[i] == vlist[j])
+					{
+						v++;
+						flag = 1;
+						break;
+					}
+				}
+				if (flag == 0)
+					c++;
 
-
+			}
+			i++;
+		}
+		*vowels = v;
+		*consonants = c;
+	}
 }
+
